8501012e63f06410f346e590bd57396565491083
Gerhard Lazu <gerhard@lazu.co.uk>
2012-02-01 15:21:06 +0000

    Access control support (allow/deny) http://wiki.nginx.org/HttpAccessModule

--- recipes/apps.rb
+++ recipes/apps.rb
@@ -3,6 +3,7 @@ node[:nginx][:apps].each do |app_name, app_attributes|
     server_port                 app_attributes[:server_port]
     server_name                 app_attributes[:server_name]
     public_path                 app_attributes[:public_path]
+    access_control              app_attributes[:access_control]
     custom_location_directives  app_attributes[:custom_location_directives]
     proxy_type                  app_attributes[:proxy_type]
     upstream_servers            app_attributes[:upstream_servers]
--- resources/app.rb
+++ resources/app.rb
@@ -4,6 +4,7 @@ attribute :name,                        :kind_of => String,  :name_attribute => 
 attribute :server_port,                 :kind_of => Fixnum,  :default => 80
 attribute :server_name,                 :kind_of => String
 attribute :public_path,                 :kind_of => String
+attribute :access_control,              :kind_of => Array,   :default => []
 attribute :custom_location_directives,  :kind_of => Array,   :default => []
 attribute :proxy_type,                  :kind_of => String,  :default => "proxy_pass",  :equal_to => %w[proxy_pass uwsgi_pass]
 attribute :upstream_servers,            :kind_of => Array,   :required => true
