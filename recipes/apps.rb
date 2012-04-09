b832c10f243606a8b31bbeeb3ba64a767b1f6acd
Gerhard Lazu <gerhard@lazu.co.uk>
2012-04-09 17:35:16 +0100

    More flexible vhosts, back-end agnostic Needs testing before merging

--- recipes/apps.rb
+++ recipes/apps.rb
@@ -1,14 +1,15 @@
 node[:nginx][:apps].each do |app_name, app_attributes|
   nginx_app app_name do
-    server_port                 app_attributes[:server_port]
-    server_name                 app_attributes[:server_name]
-    public_path                 app_attributes[:public_path]
-    access_control              app_attributes[:access_control]
-    custom_location_directives  app_attributes[:custom_location_directives]
-    proxy_type                  app_attributes[:proxy_type]
-    upstream_servers            app_attributes[:upstream_servers]
-    proxy_headers               app_attributes[:proxy_headers]
-    custom_directives           app_attributes[:custom_directives]
-    action                      app_attributes[:action]
+    server_port           app_attributes[:server_port]
+    server_name           app_attributes[:server_name]
+    public_path           app_attributes[:public_path]
+    locations             app_attributes[:locations]
+    upstream_servers      app_attributes[:upstream_servers]
+    try_files             app_attributes[:try_files]
+    error_page            app_attributes[:error_page]
+    client_max_body_size  app_attributes[:client_max_body_size]
+    keepalive_timeout     app_attributes[:keepalive_timeout]
+    custom_directives     app_attributes[:custom_directives]
+    action                app_attributes[:action]
   end
 end
--- resources/app.rb
+++ resources/app.rb
@@ -1,15 +1,16 @@
 actions :add, :remove
 
-attribute :name,                        :kind_of => String,  :name_attribute => true
-attribute :server_port,                 :kind_of => Fixnum,  :default => 80
-attribute :server_name,                 :kind_of => String
-attribute :public_path,                 :kind_of => String,  :default => "/usr/share/nginx/www"
-attribute :access_control,              :kind_of => Array,   :default => []
-attribute :custom_location_directives,  :kind_of => Array,   :default => []
-attribute :proxy_type,                  :kind_of => String,  :default => "proxy_pass",           :equal_to => %w[proxy_pass uwsgi_pass]
-attribute :upstream_servers,            :kind_of => Array,   :default => []
-attribute :proxy_headers,               :kind_of => Hash,    :default => {}
-attribute :custom_directives,           :kind_of => Array,   :default => []
+attribute :name,                  :kind_of => String,  :name_attribute => true
+attribute :server_port,           :kind_of => Fixnum,  :default => 80
+attribute :server_name,           :kind_of => String
+attribute :public_path,           :kind_of => String,  :default => "/usr/share/nginx/www"
+attribute :locations,             :kind_of => Array,   :default => []
+attribute :upstream_servers,      :kind_of => Array,   :default => []
+attribute :try_files,             :kind_of => Array,   :default => [] # $uri/index.html $uri "@#{@app.name}"
+attribute :error_page,            :kind_of => String,  :default => "/500.html"
+attribute :client_max_body_size,  :kind_of => String,  :default => "4G"
+attribute :keepalive_timeout,     :kind_of => Fixnum,  :default => 10
+attribute :custom_directives,     :kind_of => Array,   :default => []
 
 def initialize(*args)
   super
