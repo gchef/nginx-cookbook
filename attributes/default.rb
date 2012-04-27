94e62381805ba8d74076915200f546f643bd4b00
Gerhard Lazu <gerhard@lazu.co.uk>
2012-04-27 13:20:00 +0100

    worker_connections and worker_tlimit_nofile adjustments

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,15 +1,55 @@
-default[:nginx][:version]            = "1.2.0"
+default[:nginx][:version]              = "1.2.0"
 
-set[:nginx][:dir]                    = "/etc/nginx"
-set[:nginx][:log_dir]                = "/var/log/nginx"
-set[:nginx][:binary]                 = "/usr/sbin/nginx"
+default[:nginx][:dir]                  = "/etc/nginx"
+default[:nginx][:log_dir]              = "/var/log/nginx"
+default[:nginx][:binary]               = "/usr/sbin/nginx"
 
-set[:nginx][:user]                   = "www-data"
-default[:nginx][:worker_processes]   = cpu[:total]
-default[:nginx][:pid]                = "/var/run/nginx.pid"
+default[:nginx][:user]                 = "www-data"
 
-default[:nginx][:worker_connections] = 1024
-default[:nginx][:multi_accept]       = "off"
+# A worker process is a single-threaded process.
+#
+# If Nginx is doing CPU-intensive work such as SSL or gzipping and you have 2
+# or more CPUs/cores, then you may set worker_processes to be equal to the
+# number of CPUs or cores.
+#
+# 1If you are serving a lot of static files and the total size of the files is
+# bigger than the available memory, then you may increase worker_processes to
+# fully utilize disk bandwidth.
+#
+# Your OS may schedule all workers on single CPU/core this can be avoided using
+# worker_cpu_affinity.
+#
+# Nginx has the ability to use more than one worker process for several
+# reasons:
+#   * to use SMP 
+#   * to decrease latency when workers blockend on disk I/O
+#   * to limit number of connections per process when select()/poll() is used
+#
+# The worker_processes and worker_connections from the event sections allows
+# you to calculate maxclients value:
+#
+#   max_clients = worker_processes * worker_connections
+#
+default[:nginx][:worker_processes]     = cpu[:total]
+#
+# The worker_connections and worker_processes from the main section allows you
+# to calculate max clients you can handle:
+#
+#   max clients = worker_processes * worker_connections
+#
+# In a reverse proxy situation, max clients becomes
+#
+#   max clients = worker_processes * worker_connections/4
+# Since a browser opens 2 connections by default to a server and nginx uses the
+# fds (file descriptors) from the same pool to connect to the upstream backend,
+default[:nginx][:worker_connections]   = 1024
+#
+# Specifies the value for maximum file descriptors that can be opened by this
+# process.
+default[:nginx][:worker_rlimit_nofile] = 1024
+default[:nginx][:pid]                  = "/var/run/nginx.pid"
+
+default[:nginx][:multi_accept]         = "off"
 
 # Directive sets the read timeout for the request body from client. The timeout
 # is set only if a body is not get in one readstep. If after this time the
