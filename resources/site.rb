483fddbb125d355cecab6e56af4f38aa0abd1c69
Gerhard Lazu <gerhard@lazu.co.uk>
2013-03-16 15:29:29 +0000

    Using the new default_action method

--- resources/site.rb
+++ resources/site.rb
@@ -1,9 +1,5 @@
 actions :enable, :disable
+default_action :enable
 
 attribute :name,      :kind_of => String,  :name_attribute => true
 attribute :enabled,   :default => false
-
-def initialize(*args)
-  super
-  @action = :enable
-end
