106a3b62a69ff5ed8131908cad714b0ac2a37acc
Gerhard Lazu <gerhard@lazu.co.uk>
2013-03-16 15:45:19 +0000

    Avoiding all that crazy repetition, proxying through to the app provider everything that has been set in the role/environment.

--- recipes/apps.rb
+++ recipes/apps.rb
@@ -1,19 +1,7 @@
 node[:nginx][:apps].each do |app_name, app_attributes|
   nginx_app app_name do
-    access_log            app_attributes[:access_log]
-    access_log_format     app_attributes[:access_log_format]
-    action                app_attributes[:action]
-    client_max_body_size  app_attributes[:client_max_body_size]
-    custom_directives     app_attributes[:custom_directives]
-    error_log             app_attributes[:error_log]
-    error_log_format      app_attributes[:error_log_format]
-    keepalive_timeout     app_attributes[:keepalive_timeout]
-    listen                app_attributes[:listen]
-    locations             app_attributes[:locations]
-    public_path           app_attributes[:public_path]
-    server_name           app_attributes[:server_name]
-    try_files             app_attributes[:try_files]
-    upstream_keepalive    app_attributes[:upstream_keepalive]
-    upstreams             app_attributes[:upstreams]
+    app_attributes.each do |attribute, value|
+      instance_variable_set("@#{attribute}", value)
+    end
   end
 end
