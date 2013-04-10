b609582a70fa4795ab9425c6f6182d1b01b1b108
Gerhard Lazu <gerhard@lazu.co.uk>
2013-04-10 17:06:17 +0100

    Patch version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Gerhard Lazu"
 maintainer_email  "gerhard@lazu.co.uk"
 license           "Apache 2.0"
 description       "Installs and configures nginx"
-version           "2.7.4"
+version           "2.8.1"
 
 recipe "nginx", "Installs nginx package and sets up configuration with Debian apache style with sites-enabled/sites-available"
 recipe "nginx::source", "Installs nginx from source and sets up configuration with Debian apache style with sites-enabled/sites-available"
