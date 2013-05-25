3dce716d637c34e230aba987744f77c934dc338a
Gerhard Lazu <gerhard@lazu.co.uk>
2013-05-25 20:29:45 +0100

    Unless this gets restarted, new sites won't be picked up correctly

--- providers/app.rb
+++ providers/app.rb
@@ -8,7 +8,7 @@ action :add do
     variables(
       :app => new_resource
     )
-    notifies :reload, resources(:service => "nginx"), :delayed
+    notifies :restart, resources(:service => "nginx"), :delayed
   end
 
   nginx_site new_resource.name
@@ -25,12 +25,12 @@ end
 def remove
   bash "Delete nginx vhosts & logs for #{new_resource.name}" do
     code "rm -f #{node[:nginx][:log_dir]}/#{new_resource.name}* #{node[:nginx][:dir]}/sites*/#{new_resource.name}"
-    notifies :reload, resources(:service => "nginx"), :delayed
+    notifies :restart, resources(:service => "nginx"), :delayed
   end
 end
 
 def load_current_resource
   service "nginx" do
-    supports :reload => true
+    supports :restart => true
   end
 end
