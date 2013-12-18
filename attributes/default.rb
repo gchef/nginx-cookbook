34da27a907bbe9a786aca0b4c7d28603084dbaf0
Gerhard Lazu <gerhard@lazu.co.uk>
2013-12-18 14:35:18 +0000

    Ubuntu 10.04 LTS support

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,5 @@
 default[:nginx][:version]           = "1.4.4"
+default[:nginx][:version]           = "1.4.1" if node[:platform_version].to_f < 12.04
 default[:nginx][:apt_packages]      = %w[nginx-common nginx-full nginx]
 
 default[:nginx][:dir]               = "/etc/nginx"
